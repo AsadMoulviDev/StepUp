1926a1f38b49d06efa8d0e3c108d9a92121a6f18
AsadMoulviDev <asad.moulvi@hotmail.com>
2023-05-20 22:25:27 +0500

    Yess

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
-      },
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+          colors:{
+            customGray: '#C4C4C4'
+
+          },
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-  },
-  plugins: [require("@tailwindcss/typography")],
+    plugins: [require('@tailwindcss/typography')],
 };
